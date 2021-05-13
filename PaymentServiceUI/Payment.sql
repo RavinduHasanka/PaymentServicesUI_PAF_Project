ea75e6a65701e715cb6cd8510219ee9e8072c8ea
Ravindu Hasanka <it19156798@my.sliit.lk>
2021-05-13 19:39:06 +0530

    Add Database

---
+++ PaymentServiceUI/Payment.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM gadget_badget.payment
+LIMIT 0, 1000
+
+-- Date: 2021-05-13 19:37
+*/
+INSERT INTO `` (`paymentID`,`amount`,`payment_date`,`card_number`,`expire_mon_yer`,`cvc`,`card_holder_name`,`type`) VALUES (1,'10000','01/05/2020','1234567890123456','05/23','770','Perera','CreditCard');
+INSERT INTO `` (`paymentID`,`amount`,`payment_date`,`card_number`,`expire_mon_yer`,`cvc`,`card_holder_name`,`type`) VALUES (3,'100','20/2/2222','1234567896543210','02/25','500','ABC','DebitCard');
+INSERT INTO `` (`paymentID`,`amount`,`payment_date`,`card_number`,`expire_mon_yer`,`cvc`,`card_holder_name`,`type`) VALUES (4,'20000','01/02/2021','1234567890123445','03/27','780','Anjana','DebitCard');
+INSERT INTO `` (`paymentID`,`amount`,`payment_date`,`card_number`,`expire_mon_yer`,`cvc`,`card_holder_name`,`type`) VALUES (5,'25000','01/02/2021','1234567890124455','03/27','768','Deen','DebitCard');
+INSERT INTO `` (`paymentID`,`amount`,`payment_date`,`card_number`,`expire_mon_yer`,`cvc`,`card_holder_name`,`type`) VALUES (6,'50000','13/03/2021','4256981526123456','04/27','788','Hasanka','CreditCard');
+INSERT INTO `` (`paymentID`,`amount`,`payment_date`,`card_number`,`expire_mon_yer`,`cvc`,`card_holder_name`,`type`) VALUES (7,'15000','20/2/2019','1234567896543210','02/25','500','ABCD','DebitCard');
